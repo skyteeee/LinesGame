1ce7b586b62573a1f2e4953d7bd742c00391c434
Skyteeee <spusi.vapus@gmail.com>
2020-04-01 15:41:44 -0500

    Ball colors are now randomly picked. Also the ball offset in the cell was fixed.

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ class Cell {
   constructor(x, y) {
   this.x = x;
   this.y = y;
-  this.ball = new Ball ('red', x, y);
+  this.ball = null;
   }
 
   drawCell (game) {
@@ -31,6 +31,10 @@ class Cell {
     game.ctx.lineTo(x, y2);
     game.ctx.stroke();
 
+    if (this.ball) {
+      this.ball.drawBall(game);
+    }
+
     //game.ctx.strokeRect(game.cellWidth*this.x, game.cellHeight*this.y, game.cellWidth, game.cellHeight);
   }
 
@@ -49,11 +53,14 @@ class Game {
     this.cellWidth = 0;
     this.cellHeight2 = 0;
     this.cellWidth2 = 0;
+    this.possibleBallColors = ['rgb(255,0,95)', 'rgb(255,91,0)' , 'rgb(255,203,1)' , 'green' ,  'blue', 'rgb(0,158,255)' , 'purple'];
     this.field = [];
     for (let y = 0; y < this.fieldHeight; y ++) {
       let row = [];
       for (let x = 0; x < this.fieldWidth; x++) {
-        row.push(new Cell(x, y));
+        let cell = new Cell(x, y);
+        cell.ball = this.createBall(x, y);
+        row.push(cell);
       }
       this.field.push(row);
 
@@ -72,6 +79,11 @@ class Game {
 
   }
 
+  createBall(x, y) {
+    let colorIdx = Math.floor(Math.random() * this.possibleBallColors.length);
+    let color = this.possibleBallColors[colorIdx];
+    return new Ball(x, y, colorIdx, color);
+  }
 
   draw () {
     for (let y of this.field) {
@@ -79,7 +91,6 @@ class Game {
         this.ctx.save();
         this.ctx.translate(this.cellWidth * cell.x + this.cellWidth2, this.cellHeight * cell.y + this.cellHeight2);
         cell.drawCell(this);
-        cell.ball.drawBall('red', this);
         this.ctx.restore();
       }
     }
@@ -88,29 +99,26 @@ class Game {
 }
 
 class Ball {
-  constructor(x, y) {
-    this.possibleColors = ['red', 'orange' , 'yellow' , 'green' ,  'light blue', 'blue', 'purple'];
-
+  constructor(x, y, colorIdx, color) {
+    this.colorIdx = colorIdx;
+    this.color = color;
     this.x = x;
     this.y = y;
   }
 
-  drawBall (color, game) {
+  drawBall (game) {
     let x = -game.cellWidth2;
     let y = -game.cellHeight2;
 
-    game.ctx.save();
-    game.ctx.translate(this.x, this.y);
-    let gradient = game.ctx.createRadialGradient(-15, -15, 0, -5, -5, 25);
+    let gradient = game.ctx.createRadialGradient(-12, -12, 0, 0, 0, 25);
 
     gradient.addColorStop(0, 'white');
-    gradient.addColorStop(0.7, color);
-    gradient.addColorStop(0.9, color);
+    gradient.addColorStop(0.7, this.color);
+    gradient.addColorStop(0.9, this.color);
     gradient.addColorStop(1, 'transparent');
 
     game.ctx.fillStyle = gradient;
     game.ctx.fillRect(x, y, game.cellWidth, game.cellHeight);
-    game.ctx.restore();
 
   }
 
